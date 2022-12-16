c7647f31f51e9b0bfb8b04f420381959066d3cd6
Alexander <painkillerqq@gmai.com>
2022-12-16 19:53:07 +0700

    password generating

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,25 +3,25 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
-import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
 import password.Password
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-//        val passTextView: TextView = findViewById(R.id.pass)
-//        passTextView.text = Password().generatePass(8)
+        binding.passwordValueTextView.text = Password().generatePass(8)
 
-        val passLength: TextView = findViewById(R.id.pass_length_value)
-        val passLengthSeekBar: SeekBar = findViewById(R.id.pass_length_seekbar)
-
-        passLengthSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+        binding.passwordLengthSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                val lengthOfPass = passLengthSeekBar.progress
-                passLength.text = lengthOfPass.toString()
-//                passTextView.text = Password().generatePass(lengthOfPass)
+                val lengthOfPass = binding.passwordLengthSeekBar.progress
+                binding.passwordLengthValueTextView.text = lengthOfPass.toString()
+                binding.passwordValueTextView.text = Password().generatePass(lengthOfPass)
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
