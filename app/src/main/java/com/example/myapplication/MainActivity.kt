a7f2f1245df5e634422795e5e470f22146121ddf
Alexander <painkillerqq@gmai.com>
2022-12-16 16:48:47 +0700

    design improvement

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import android.widget.TextView
+import password.Password
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.pass_length_value)
-        val seekBar: SeekBar = findViewById(R.id.pass_length_seekbar)
-        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+//        val passTextView: TextView = findViewById(R.id.pass)
+//        passTextView.text = Password().generatePass(8)
+
+        val passLength: TextView = findViewById(R.id.pass_length_value)
+        val passLengthSeekBar: SeekBar = findViewById(R.id.pass_length_seekbar)
+
+        passLengthSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                textView.text = seekBar.progress.toString()
+                val lengthOfPass = passLengthSeekBar.progress
+                passLength.text = lengthOfPass.toString()
+//                passTextView.text = Password().generatePass(lengthOfPass)
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
